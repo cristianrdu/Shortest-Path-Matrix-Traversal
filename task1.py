d0753e3d7393eb13b7ab0d825d587bda492f1b61
Cristi <chris54332@gmail.com>
2020-12-15 21:38:11 +0200

    Fixed the ACO bug, added in the heuristic algo.

--- task1.py
+++ task1.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import numpy as np
 import random
 import math
@@ -9,7 +11,34 @@ input1 = np.array([[0, 6, 4, 5],
                    [9, 6, 1, 2],
                    [2, 3, 4, 5]])
 
-input2 = np.array([[0, 6, 4, 5, 1, 4, 3, 5, 6, 8, 7],
+input2_v1 = np.array([[1, 9, 9, 9, 9, 9],
+                      [1, 1, 0, 1, 0, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 1, 1, 9, 1]])
+
+input2_v2 = np.array([[1, 9, 9, 9, 9, 9],
+                      [1, 1, 1, 1, 1, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 9, 1, 9, 1],
+                      [9, 1, 1, 1, 9, 1]])
+
+input3 = np.array([[1, 6, 4, 5, 1, 4, 3, 6, 8, 7],
+                   [1, 3, 3, 9, 1, 4, 3, 6, 2, 1],
+                   [4, 1, 9, 1, 1, 4, 3, 6, 5, 3],
+                   [9, 6, 1, 2, 1, 4, 3, 6, 2, 1],
+                   [1, 3, 5, 4, 1, 4, 3, 6, 8, 4],
+                   [8, 7, 2, 9, 1, 4, 3, 6, 7, 5],
+                   [8, 7, 2, 9, 1, 4, 3, 6, 7, 5],
+                   [1, 6, 3, 5, 1, 4, 3, 6, 2, 2],
+                   [8, 7, 2, 9, 1, 4, 3, 6, 7, 5],
+                   [1, 6, 3, 5, 1, 4, 3, 6, 2, 2]])
+
+input4 = np.array([[0, 6, 4, 5, 1, 4, 3, 5, 6, 8, 7],
                    [1, 3, 3, 9, 1, 4, 3, 5, 6, 2, 1],
                    [4, 1, 9, 1, 1, 4, 3, 5, 6, 5, 3],
                    [9, 6, 1, 2, 1, 4, 3, 5, 6, 2, 1],
@@ -21,14 +50,6 @@ input2 = np.array([[0, 6, 4, 5, 1, 4, 3, 5, 6, 8, 7],
                    [8, 7, 2, 9, 1, 4, 3, 5, 6, 7, 5],
                    [1, 6, 3, 5, 1, 4, 3, 5, 6, 2, 2]])
 
-input3 = np.array([[1, 9, 9, 9, 9, 9],
-                   [1, 1, 9, 1, 1, 1],
-                   [9, 1, 9, 1, 9, 1],
-                   [9, 1, 9, 1, 9, 1],
-                   [9, 1, 9, 1, 9, 1],
-                   [9, 1, 9, 1, 9, 1],
-                   [9, 1, 1, 1, 9, 1]])
-
 
 def ant_colony_optimization(input):
     grid = input
@@ -36,41 +57,43 @@ def ant_colony_optimization(input):
     col = grid.shape[1]
 
     pheromone_grid = np.ones((row, col))
-    gens = 50
-    evap_rate = 0.5
-    ants = 32
-    # alpha = 6
-    # shortest = [], 10000000000
+    gens = 40
+    evap_rate = 0.7
+    ants = 1200
+    alpha = 1.5
 
     def calculate_shortest_path(grid, pheromone_grid, evap_rate, gens, ants):
         shortest_path_found = []
         for generation in range(gens):
-            shortest_distance = []
+            ants_distances = []
             history = []
             interim_grid = np.zeros((row, col))
-            print("GENERATION ", generation)
+            pheromone_grid = (1 - evap_rate) * pheromone_grid + interim_grid
+            # print("GENERATION ", generation)
             for ant in range(ants):
 
                 ant_history, distance_traveled = traverse_grid(
                     grid, pheromone_grid)
 
-                shortest_distance.append(distance_traveled)
-
-                # Stacks the positions traversed by all ants while dividing the quality of the position
+                ants_distances.append(distance_traveled)
+                if distance_traveled == 0:
+                    distance_traveled = 0.7
+                # Stacks the positions traversed by all ants while dividing the alpha constant
                 # by the total distance traveled by each ant. This is the formula from Wikipedia
                 for (x, y) in ant_history:
-                    interim_grid[x][y] += grid[x][y] / distance_traveled
+
+                    interim_grid[x][y] += alpha / distance_traveled
                 # if ant == ants - 1:
                 #     print(distance_traveled)
 
-            shortest_path_found.append(np.min(shortest_distance))
+            shortest_path_found.append(np.min(ants_distances))
 
-            pheromone_grid = (1 - evap_rate) * pheromone_grid + interim_grid
+            # pheromone_grid = (1 - evap_rate) * pheromone_grid + interim_grid
 
-        print("shortest paths for each gen: ", shortest_path_found)
+        print("shortest path: ", np.min(
+            shortest_path_found + grid[row - 1][col - 1] - grid[0][0]))
 
     def traverse_grid(grid, pheromone_grid):
-        # print("NEW ant: ")
         current_node = (0, 0)
         end_node = (row - 1, col - 1)
         history = set()
@@ -78,80 +101,83 @@ def ant_colony_optimization(input):
         distance_traveled = 0
         potential_nodes = (calculate_potential_nodes(history, current_node))
 
-        while (current_node != end_node) and potential_nodes != 0:
-            # print("current node:", grid[current_node[0]]
-            #       [current_node[1]], " pos: ", current_node)
+        while (current_node != end_node) and len(potential_nodes) != 0:
             distance_traveled += grid[current_node[0]][current_node[1]]
 
-            if (current_node[0], current_node[1] + 1) == end_node or (current_node[0] + 1, current_node[1]) == end_node:
-
-                return history, distance_traveled
-
-            # print(potential_nodes)
-            potential_nodes_sum = 0
-            # calculate sum of each possible node for the probability usage
-            for (x, y) in potential_nodes:
-                if x == current_node[0] + 1:
-                    potential_nodes_sum += calculate_south(current_node)
-                elif x == current_node[0] - 1:
-                    potential_nodes_sum += calculate_north(current_node)
-                elif y == current_node[1] + 1:
-                    potential_nodes_sum += calculate_east(current_node)
-                elif y == current_node[1] - 1:
-                    potential_nodes_sum += calculate_west(current_node)
-
-            north_prob = 0
-            west_prob = 0
-            east_prob = 0
-            south_prob = 0
-            # calculate probabilities
-            for (x, y) in potential_nodes:
-                if x == current_node[0] + 1:
-                    south_prob = calculate_south(
-                        current_node)/potential_nodes_sum
-                if x == current_node[0] - 1:
-                    north_prob = calculate_north(
-                        current_node)/potential_nodes_sum
-                if y == current_node[1] + 1:
-                    east_prob = calculate_east(
-                        current_node)/potential_nodes_sum
-                if y == current_node[1] - 1:
-                    west_prob = calculate_west(
-                        current_node)/potential_nodes_sum
-
-            chosen_prob = roullete_select(
-                [north_prob, west_prob, east_prob, south_prob])
-
-            if chosen_prob == north_prob:
-                current_node = (current_node[0] - 1, current_node[1])
-            elif chosen_prob == south_prob:
-                current_node = (current_node[0] + 1, current_node[1])
-            elif chosen_prob == west_prob:
-                current_node = (current_node[0], current_node[1] - 1)
-            elif chosen_prob == east_prob:
-                current_node = (current_node[0], current_node[1] + 1)
+            current_node = choose_node(current_node, potential_nodes)
 
             history.add(current_node)
 
             potential_nodes = calculate_potential_nodes(history, current_node)
 
+        if current_node != end_node:
+            distance_traveled = 100000
+
         return history, distance_traveled
+
+    def choose_node(current_node, potential_nodes):
+        potential_nodes_sum = 0
+        # calculate sum of each possible node for the probability later
+        for (x, y) in potential_nodes:
+            if x == current_node[0] + 1:
+                potential_nodes_sum += calculate_south(current_node)
+            elif x == current_node[0] - 1:
+                potential_nodes_sum += calculate_north(current_node)
+            elif y == current_node[1] + 1:
+                potential_nodes_sum += calculate_east(current_node)
+            elif y == current_node[1] - 1:
+                potential_nodes_sum += calculate_west(current_node)
+
+        north_prob = 0
+        west_prob = 0
+        east_prob = 0
+        south_prob = 0
+        # calculate probabilities
+        for (x, y) in potential_nodes:
+            if x == current_node[0] + 1:
+                south_prob = calculate_south(
+                    current_node)/potential_nodes_sum
+            if x == current_node[0] - 1:
+                north_prob = calculate_north(
+                    current_node)/potential_nodes_sum
+            if y == current_node[1] + 1:
+                east_prob = calculate_east(
+                    current_node)/potential_nodes_sum
+            if y == current_node[1] - 1:
+                west_prob = calculate_west(
+                    current_node)/potential_nodes_sum
+
+        chosen_prob = roullete_select(
+            [north_prob, west_prob, east_prob, south_prob])
+        # roullete_select returns the index of the chosen probability
+        # in the order which they we're inputted above.
+        # For index 0 it's north_prob, for index 1 it's west_prob and so on... .
+        # This 'if' function distinguishes which potential node to return based on that index rule.
+        if chosen_prob == 0:
+            return (current_node[0] - 1, current_node[1])
+        elif chosen_prob == 1:
+            return (current_node[0], current_node[1] - 1)
+        elif chosen_prob == 2:
+            return (current_node[0], current_node[1] + 1)
+        elif chosen_prob == 3:
+            return (current_node[0] + 1, current_node[1])
 
     def calculate_potential_nodes(history, curr_node):
         a = set()
-        if curr_node[1] < col - 1 and curr_node[1] > 0:
-            if curr_node[0] - 1 >= 0 and (curr_node[0] - 1, curr_node[1]) not in history:
-
-                a.add((curr_node[0] - 1, curr_node[1]))
 
         if curr_node[0] + 1 <= row - 1 and (curr_node[0] + 1, curr_node[1]) not in history:
 
             a.add((curr_node[0] + 1, curr_node[1]))
 
-        if curr_node[0] < row - 1 and curr_node[0] > 0:
-            if curr_node[1] - 1 >= 0 and (curr_node[0], curr_node[1] - 1) not in history:
+        # if curr_node[1] < col - 1 and curr_node[1] > 0:
+        if curr_node[0] - 1 >= 0 and (curr_node[0] - 1, curr_node[1]) not in history:
 
-                a.add((curr_node[0], curr_node[1] - 1))
+            a.add((curr_node[0] - 1, curr_node[1]))
+
+        # if curr_node[0] < row - 1 and curr_node[0] > 0:
+        if curr_node[1] - 1 >= 0 and (curr_node[0], curr_node[1] - 1) not in history:
+
+            a.add((curr_node[0], curr_node[1] - 1))
 
         if curr_node[1] + 1 <= col - 1 and (curr_node[0], curr_node[1] + 1) not in history:
 
@@ -160,44 +186,95 @@ def ant_colony_optimization(input):
         return a
 
     def roullete_select(probabilities):
-        # Sorting the probabilities in case there are any zero probabilities.
-        # This happens when the current point has reached one or more walls
-        # and has no potential node to calculate probability for, therefore the zero probabilities
-        probabilities.sort()
         r = random.uniform(0, 1)
         sum_index = 0
-        chosen_prob = 0
-
+        # chosen_prob = 0
+        chosen_prob_index = 0
         # sum_index acts as the cummulative sum as it increments
         #  without any need for extra storage use
-        for i in probabilities:
-            if sum_index <= r:
-                chosen_prob = i
-            sum_index += i
+        for i in range(len(probabilities)):
+            # Skipping the probabilities in case there are any zero probabilities.
+            # This happens when the current point has reached one or more walls
+            # and has no potential node to calculate probability for.
+            if probabilities[i] == 0:
+                continue
 
-        return chosen_prob
+            if sum_index <= r:
+                chosen_prob_index = i
+
+            sum_index += probabilities[i]
+
+        return chosen_prob_index
 
     def calculate_north(a):
-        north = pheromone_grid[a[0] - 1][a[1]]*(1/grid[a[0] - 1][a[1]])
-        return north
+        if grid[a[0] - 1][a[1]] == 0:
+            # return pheromone_grid[a[0] - 1][a[1]]
+            return 0.49
+        return pheromone_grid[a[0] - 1][a[1]]*(1/grid[a[0] - 1][a[1]])
 
     def calculate_west(a):
-        west = pheromone_grid[a[0]][a[1] - 1]*(1/grid[a[0]][a[1] - 1])
-        return west
+        if grid[a[0]][a[1] - 1] == 0:
+            # return pheromone_grid[a[0]][a[1] - 1]
+            return 0.49
+        return pheromone_grid[a[0]][a[1] - 1]*(1/grid[a[0]][a[1] - 1])
 
     def calculate_east(a):
+        if grid[a[0]][a[1] + 1] == 0:
+            # return pheromone_grid[a[0]][a[1] + 1]
+            return 0.49
         east = pheromone_grid[a[0]][a[1] + 1]*(1/grid[a[0]][a[1] + 1])
         return east
 
     def calculate_south(a):
-        south = pheromone_grid[a[0] + 1][a[1]]*(1/grid[a[0] + 1][a[1]])
-        return south
+        if grid[a[0] + 1][a[1]] == 0:
+            # return pheromone_grid[a[0] + 1][a[1]]
+            return 0.49
+        return pheromone_grid[a[0] + 1][a[1]]*(1/grid[a[0] + 1][a[1]])
 
     calculate_shortest_path(grid, pheromone_grid, evap_rate, gens, ants)
 
 
-# def djikstra(input):
-#     grid = input
+def heuristic(input):
+    # np.random.seed(0)
+    size_x = 5
+    size_y = 5
+    random_matrix = input
+
+    time_spent = random_matrix[0, 0]
+
+    x = y = 0
+
+    while x < size_x and y < size_y:
+        print((x, y), random_matrix[x, y])
+
+        if x < size_x and y < size_y:
+            look_right = random_matrix[x, y + 1]
+            look_down = random_matrix[x + 1, y]
+
+            if look_right == min(look_right, look_down):
+                time_spent += look_right
+                y += 1
+            else:
+                time_spent += look_down
+                x += 1
+
+        if size_x == x and y < size_y:
+            time_spent += random_matrix[x, y + 1]
+            y += 1
+
+        if size_y == y and x < size_x:
+            time_spent += random_matrix[x + 1, y]
+            x += 1
+
+    print((x, y), random_matrix[x, y])
+    print(time_spent)
+    print(random_matrix)
 
 
-ant_colony_optimization(input1)
+def djikstra(input):
+    start = (0, 0)
+
+
+rand_input = np.random.randint(10, size=(5, 5))
+# print(rand_input)
+ant_colony_optimization(input4)
